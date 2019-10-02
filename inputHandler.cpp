da4a35ca0d239db7ed7f1edefe5c109b2d533b03
Lorenzo Theunissen <l.s.j.theunissen@students.uu.nl>
2019-10-02 16:41:10 +0200

    return values

--- inputHandler.cpp
+++ inputHandler.cpp
@@ -2,19 +2,22 @@
 
 vec2 InputHandler::MousePos()
 {
-
+	return vec2(0,0);
 }
 
 bool InputHandler::MouseLeftDown()
 {
-
+	return false;
 }
 bool InputHandler::MouseLeftUp()
 {
+	return false;
 }
 bool InputHandler::MouseRightDown()
 {
+	return false;
 }
 bool InputHandler::MouseRightUp()
 {
+	return false;
 }
