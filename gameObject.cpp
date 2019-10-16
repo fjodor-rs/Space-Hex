db826db87993bca7641e27e1ef8d4d8aa0776945
Joris <joris.l@hotmail.com>
2019-10-16 16:59:09 +0200

    gameobject chain

--- gameObject.cpp
+++ gameObject.cpp
@@ -1,5 +1,10 @@
 #include "precomp.h"
 
+GameObject::GameObject()
+{
+	
+}
+
 void GameObject::Init()
 {
 
@@ -7,15 +12,21 @@ void GameObject::Init()
 
 void GameObject::Shutdown()
 {
-
+	next->prev = prev->next;
+	prev->next = next->prev;
 }
 
 void GameObject::Update(float deltatime)
 {
 
+
+	if ( next != NULL )
+		next->Update( deltatime );
 }
 
 void GameObject::HandleInput()
 {
 
+	if ( next != NULL )
+		next->HandleInput();
 }
--- gameObject.h
+++ gameObject.h
@@ -7,10 +7,12 @@ class GameObject
 {
 
   public:
+	GameObject();
 	virtual void Init();
 	virtual void Shutdown();
 	virtual void Update( float deltatime );
 	virtual void HandleInput();
+	GameObject *next, *prev;
 };
 
 }; // namespace Tmpl8
